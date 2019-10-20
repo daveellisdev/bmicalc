1c9bec1d75f654088fdbf4367f8f4a93e13f82af
Dave Ellis <dave@daveellis.dev>
2019-10-20 10:02:04 +0100

    Updated for code styling

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // VIEW CONTROLLER 
 
-let ViewController = (function() {
+let ViewController = (function () {
 
     let DOMStrings = {
         heightButton: ".height-switch",
@@ -39,45 +39,45 @@ let ViewController = (function() {
 
     return {
 
-        getEl: function(str){
+        getEl: function (str) {
             return document.querySelector(str);
         },
 
-        getDOMStrings: function() {
+        getDOMStrings: function () {
             return DOMStrings;
         },
 
         updateResults(results) {
             let position;
             let cat;
-            if(isNaN(results.bmi)){
+            if (isNaN(results.bmi)) {
                 this.getEl(DOMStrings.resultBMI).innerText = `---`;
                 this.getEl(DOMStrings.arrow).style.cssText = `left: calc(50% - 10px)`;
             } else {
                 this.getEl(DOMStrings.resultBMI).innerText = results.bmi;
             }
-            if(isNaN(results.ratio)){
+            if (isNaN(results.ratio)) {
                 this.getEl(DOMStrings.resultWaist).innerText = `---`
             } else {
                 this.getEl(DOMStrings.resultWaist).innerText = results.ratio;
             }
 
             position = (results.bmi - 15) * 5;
-            if(position<0){
+            if (position < 0) {
                 position = 0;
-            } else if (position > 100){
+            } else if (position > 100) {
                 position = 100;
             }
-            
+
             this.getEl(DOMStrings.arrow).style.cssText = `left: calc(${position}% - 10px)`;
 
-            if(results.bmi <= 18.5){
+            if (results.bmi <= 18.5) {
                 cat = "Underweight";
-            } else if(results.bmi > 18.5 && results.bmi < 25) {
+            } else if (results.bmi > 18.5 && results.bmi < 25) {
                 cat = "Healthy"
-            } else if(results.bmi >= 25 && results.bmi <= 29.9) {
+            } else if (results.bmi >= 25 && results.bmi <= 29.9) {
                 cat = "Overweight"
-            } else if(results.bmi >= 30) {
+            } else if (results.bmi >= 30) {
                 cat = "Obese"
             } else {
                 cat = "---"
@@ -85,18 +85,18 @@ let ViewController = (function() {
             this.getEl(DOMStrings.resultBMICat).innerText = cat;
         },
 
-        updateMessage: function(warning) {
-            if(!warning){
+        updateMessage: function (warning) {
+            if (!warning) {
                 this.getEl(DOMStrings.msg).innerText = "Please enter numbers.";
             } else {
                 this.getEl(DOMStrings.msg).innerText = "";
             }
         },
 
-        updateMetric: function(unitType, metricSetting){
-            switch(unitType){
+        updateMetric: function (unitType, metricSetting) {
+            switch (unitType) {
                 case 0:
-                    if(metricSetting){
+                    if (metricSetting) {
                         this.getEl(DOMStrings.heightUnits1).innerText = unitsSet[1][0][0];
                         this.getEl(DOMStrings.heightInput1).placeholder = "1";
                         this.getEl(DOMStrings.heightInput1).min = "0";
@@ -117,7 +117,7 @@ let ViewController = (function() {
                     }
                     break;
                 case 1:
-                    if(metricSetting){
+                    if (metricSetting) {
                         this.getEl(DOMStrings.weightUnits1).innerText = unitsSet[1][1][0];
                         this.getEl(DOMStrings.weightUnits2).innerText = unitsSet[1][1][1];
                         this.getEl(DOMStrings.weightInput1).placeholder = "75";
@@ -130,7 +130,7 @@ let ViewController = (function() {
                     }
                     break;
                 case 2:
-                    if(metricSetting){
+                    if (metricSetting) {
                         this.getEl(DOMStrings.waistUnits).innerText = unitsSet[1][2][0];
                         this.getEl(DOMStrings.waistInput).placeholder = "50";
                     } else {
@@ -138,14 +138,14 @@ let ViewController = (function() {
                         this.getEl(DOMStrings.waistInput).placeholder = "20";
                     }
                     break;
-            }            
+            }
         },
     }
 })();
 
 // MODEL CONTROLLER
 
-let ModelController = (function() {
+let ModelController = (function () {
 
     let values = {
         weight: 0,
@@ -162,7 +162,7 @@ let ModelController = (function() {
     let metric = [false, false, false];
 
     return {
-        initValues: function(){
+        initValues: function () {
             values.weight = 0;
             values.height = 0;
             values.waist = 0;
@@ -171,28 +171,28 @@ let ModelController = (function() {
             metric = [false, false, false];
         },
 
-        calcResults(){
+        calcResults() {
             results.bmi = (values.weight / (values.height * values.height)).toFixed(2);
             results.ratio = (values.waist / values.height).toFixed(2);
         },
 
-        getResults(){
+        getResults() {
             return results;
         },
 
-        getValues: function(){
+        getValues: function () {
             return values;
         },
 
-        getMetric: function(){
+        getMetric: function () {
             return metric;
         },
 
-        setMetric: function(updatedMetric){
+        setMetric: function (updatedMetric) {
             metric = updatedMetric;
         },
 
-        setValues: function(newValues){
+        setValues: function (newValues) {
             values = newValues;
         }
     }
@@ -200,17 +200,17 @@ let ModelController = (function() {
 
 // APP CONTROLLER
 
-let AppController = (function(ViewCtrl, ModelCtrl){
+let AppController = (function (ViewCtrl, ModelCtrl) {
 
     let DOM = ViewCtrl.getDOMStrings();
 
-    let addMetricSwitch = function(){
+    let addMetricSwitch = function () {
         ViewCtrl.getEl(DOM.heightButton).selector = 0; // IDs Height
         ViewCtrl.getEl(DOM.weightButton).selector = 1; // IDs Weight
         ViewCtrl.getEl(DOM.waistButton).selector = 2; // IDs Waist
     };
 
-    let setupEventListeners = function(){
+    let setupEventListeners = function () {
         ViewCtrl.getEl(DOM.heightButton).addEventListener("click", switchMetric);
         ViewCtrl.getEl(DOM.weightButton).addEventListener("click", switchMetric);
         ViewCtrl.getEl(DOM.waistButton).addEventListener("click", switchMetric);
@@ -218,22 +218,22 @@ let AppController = (function(ViewCtrl, ModelCtrl){
 
         // selector for keypress enter
         document.querySelectorAll("input").forEach(item => {
-            item.addEventListener("keypress", function(e){
-                if(e.key == "Enter"){
+            item.addEventListener("keypress", function (e) {
+                if (e.key == "Enter") {
                     collectInputs();
                 }
             });
         });
     };
 
-    let switchMetric = function(e) {
+    let switchMetric = function (e) {
         //get metric state info
         let metric = ModelCtrl.getMetric();
         let unitType = e.target.selector;
         let metricSetting = metric[unitType];
 
         //flip metric state for relevant unit
-        metricSetting =!metricSetting;
+        metricSetting = !metricSetting;
         metric[unitType] = metricSetting;
 
         //update model with new state
@@ -242,8 +242,8 @@ let AppController = (function(ViewCtrl, ModelCtrl){
         //update UI
         ViewCtrl.updateMetric(unitType, metricSetting)
     };
-    
-    let collectInputs = function() {
+
+    let collectInputs = function () {
         let height1, height2, weight1, weight2, waist, fullHeight, fullWeight, fullWaist;
         let metricSetting = ModelCtrl.getMetric();
         let values = ModelCtrl.getValues();
@@ -252,41 +252,41 @@ let AppController = (function(ViewCtrl, ModelCtrl){
         height2 = parseInt(ViewCtrl.getEl(DOM.heightInput2).value);
         weight1 = parseInt(ViewCtrl.getEl(DOM.weightInput1).value);
         weight2 = parseInt(ViewCtrl.getEl(DOM.weightInput2).value);
-        if(isNaN(weight2)){
+        if (isNaN(weight2)) {
             weight2 = 0;
         }
         waist = parseInt(ViewCtrl.getEl(DOM.waistInput).value);
 
-        if(metricSetting[0] === false){
+        if (metricSetting[0] === false) {
             height1 = height1 / 3.281;
-            height2 = height2 * 2.54; 
+            height2 = height2 * 2.54;
         }
 
-        fullHeight = height1 + (height2/100);
+        fullHeight = height1 + (height2 / 100);
 
-        if(metricSetting[1] === false){
+        if (metricSetting[1] === false) {
             weight1 = weight1 * 6.35;
-            weight2 = weight2 * 453.592; 
+            weight2 = weight2 * 453.592;
         }
 
-        fullWeight = weight1 + (weight2/1000);
+        fullWeight = weight1 + (weight2 / 1000);
 
-        if(metricSetting[2] === false){
+        if (metricSetting[2] === false) {
             waist = (waist * 2.54) / 100;
         }
 
         fullWaist = waist;
-        
+
         values.height = fullHeight;
         values.weight = fullWeight;
         values.waist = fullWaist;
 
-        if(isNaN(values.height) || isNaN(values.weight)){
+        if (isNaN(values.height) || isNaN(values.weight)) {
             ViewCtrl.updateMessage(false);
         } else {
             ViewCtrl.updateMessage(true);
         }
-        
+
         ModelCtrl.setValues(values);
         ModelCtrl.calcResults();
 
@@ -295,7 +295,7 @@ let AppController = (function(ViewCtrl, ModelCtrl){
     }
 
     return {
-        init: function(){
+        init: function () {
             console.log('Application Started');
 
             // add metric switch to objects
